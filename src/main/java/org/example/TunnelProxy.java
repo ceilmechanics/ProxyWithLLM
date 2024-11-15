befddef01217ffe3fbb028f2d5935a3ce7ac2ba6
ceilmechanics <wendy.personal.1213@gmail.com>
2024-11-14 22:08:46 -0500

    added tunnelview

--- src/main/java/org/example/TunnelProxy.java
+++ src/main/java/org/example/TunnelProxy.java
@@ -1,8 +1,13 @@
 package org.example;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 //
@@ -11,113 +16,47 @@ public class TunnelProxy extends ChannelInboundHandlerAdapter {
     private ChannelFuture cf;
     private String host;
     private int serverPort;
-    private int port;
+//    private int port;
 
     public TunnelProxy(int port) {
         serverPort = port;
+        tunnelDriver();
     }
-//
-//    @Override
-//    public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
-//        if (msg instanceof FullHttpRequest) {
-//            FullHttpRequest request = (FullHttpRequest) msg;
-//            String host = request.headers().get("host");
-//            String[] hostInfo = host.split(":");
-//
-//            // processing host & port
-//            int port = 80;
-//            if (hostInfo.length > 1) {
-//                port = Integer.parseInt(hostInfo[1]);
-//            }
-//            else if (request.uri().indexOf("https") == 0){
-//                port = 443;
-//            }
-//            this.host = hostInfo[0];
-//            this.port = port;
-//
-//            if ("CONNECT".equalsIgnoreCase(request.method().name())) {
-//                System.out.println("ProxyServerHandler >>>> CONNECT request received.");
-//
-//                // SSL handshake, establishing a secure connection
-//                SslContext sslCtx = SslContextBuilder.forServer(
-//                        Certificate.theCertificate().getServerPrivateKey(),
-//                        Certificate.theCertificate().getCertificate(this.host)
-//                ).build();
-//
-//                // proxy will server as a tunnel, response with 200
-//                HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-//                ctx.writeAndFlush(response);
-//                ctx.pipeline().remove("httpCodec");
-//                ctx.pipeline().remove("httpObject");
-//            }
-//            else if ("GET".equalsIgnoreCase(request.method().name())){
-//                System.out.println("ProxyServerHandler >>>> GET request received.");
-//
-//                // Proxy, as a client, talks to the real server
-//                Bootstrap bootstrap = new Bootstrap();
-//                bootstrap.group(ctx.channel().eventLoop()) // register thread pool
-//                        .channel(ctx.channel().getClass())
-//                        .handler(new HttpProxyInitializer(ctx.channel()));
-//
-//                ChannelFuture httpcf = bootstrap.connect(host, port);
-//                httpcf.addListener(new ChannelFutureListener() {
-//                    public void operationComplete(ChannelFuture future) throws Exception {
-//                        if (future.isSuccess()) {
-//                            System.out.println("http cfListener >>>> connection with server established, writing to the target server...");
-//                            future.channel().writeAndFlush(msg);
-//                        } else {
-//                            ctx.channel().close();
-//                        }
-//                    }
-//                });
-//            }
-//            else {
-//                // it is not a GET request, close connection
-//                System.out.println("ProxyServerHandler >>>> invalid request method: " + request.method().name());
-//                ctx.channel().close();
-//            }
-//        }
-//        // private key only exists on client side & real server
-//        // therefore, what proxy can do at this moment is actually forward client data to server
-//        // and send server's data back to client
-//        else {
-//            if (cf == null) {
-//                // build a connection
-//                Bootstrap bootstrap = new Bootstrap();
-//                bootstrap.group(ctx.channel().eventLoop())
-//                        .channel(ctx.channel().getClass())
-//                        .handler(new ChannelInitializer() {
-//
-//                            @Override
-//                            protected void initChannel(Channel ch) throws Exception {
-//                                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
-//                                    @Override
-//                                    public void channelRead(ChannelHandlerContext ctx0, Object msg) throws Exception {
-//                                        System.out.println("HTTPS >>>> forwarding server's response back to client " +
-//                                                ctx.channel().remoteAddress());
-//                                        ctx.channel().writeAndFlush(msg);
-//                                    }
-//                                });
-//                            }
-//                        });
-//                cf = bootstrap.connect(host, port);
-//                cf.addListener(new ChannelFutureListener() {
-//                    public void operationComplete(ChannelFuture future) throws Exception {
-//                        if (future.isSuccess()) {
-//                            System.out.println("HTTPS created cf successfully >>>> writing to " + future.channel().remoteAddress());
-//                            future.channel().writeAndFlush(msg);
-//                        } else {
-//                            ctx.channel().close();
-//                        }
-//                    }
-//                });
-//            }
-//
-//            else {
-//                System.out.println("HTTPS cf already exists >>>> writing to " + cf.channel().remoteAddress());
-//                cf.channel().writeAndFlush(msg);
-//            }
-//        }
-//    }
-//
+
+    public void tunnelDriver() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup(20);
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)  // at most 100 connections in the queue
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<Channel>() {
+
+                        @Override
+                        protected void initChannel(Channel ch) {
+                            ch.pipeline().addLast("httpRequestDecoder", new HttpRequestDecoder());
+                            ch.pipeline().addLast("httpResponseEncoder", new HttpResponseEncoder());
+                            ch.pipeline().addLast("httpAggregator", new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast("httpProxyServer", new TunnelProxyServer());
+                        }
+                    });
+
+            System.out.println("ProxyTunnel >>>> starting on port " + serverPort);
+
+            ChannelFuture f = b.bind(serverPort).sync();
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // clean up
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+
 }
---
+++ src/main/java/org/example/TunnelProxyServer.java
@@ -0,0 +1,112 @@
+package org.example;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+
+public class TunnelProxyServer extends ChannelInboundHandlerAdapter {
+    private String host;
+    private int port;
+    private ChannelFuture cf;
+
+    @Override
+    public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
+        if (msg instanceof FullHttpRequest) {
+            FullHttpRequest request = (FullHttpRequest) msg;
+            String host = request.headers().get("host");
+            String[] hostInfo = host.split(":");
+
+            // processing host & port
+            int port = 80;
+            if (hostInfo.length > 1) {
+                port = Integer.parseInt(hostInfo[1]);
+            }
+            else if (request.uri().indexOf("https") == 0){
+                port = 443;
+            }
+            this.host = hostInfo[0];
+            this.port = port;
+
+            if ("CONNECT".equalsIgnoreCase(request.method().name())) {
+                System.out.println("ProxyServerHandler >>>> CONNECT request received.");
+
+                // SSL handshake, establishing a secure connection
+                SslContext sslCtx = SslContextBuilder.forServer(
+                        Certificate.theCertificate().getServerPrivateKey(),
+                        Certificate.theCertificate().getCertificate(this.host)
+                ).build();
+
+                // proxy will server as a tunnel, response with 200
+                HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+                ctx.writeAndFlush(response);
+                ctx.pipeline().remove("httpRequestDecoder");
+                ctx.pipeline().remove("httpResponseEncoder");
+                ctx.pipeline().remove("httpAggregator");
+            }
+            else if ("GET".equalsIgnoreCase(request.method().name())){
+                System.out.println("ProxyServerHandler >>>> GET request received.");
+
+                // Proxy, as a client, talks to the real server
+                Bootstrap bootstrap = new Bootstrap();
+                bootstrap.group(ctx.channel().eventLoop()) // register thread pool
+                        .channel(ctx.channel().getClass())
+                        .handler(new ProxyClientHandler(ctx.channel()));
+
+                ChannelFuture httpcf = bootstrap.connect(host, port);
+                httpcf.addListener((ChannelFutureListener) future -> {
+                    if (future.isSuccess()) {
+                        System.out.println("http cfListener >>>> connection with server established, writing to the target server...");
+                        future.channel().writeAndFlush(msg);
+                    } else {
+                        ctx.channel().close();
+                    }
+                });
+            }
+            else {
+                // it is not a GET request, close connection
+                System.out.println("ProxyServerHandler >>>> invalid request method: " + request.method().name());
+                ctx.channel().close();
+            }
+        }
+        // private key only exists on client side & real server
+        // therefore, what proxy can do at this moment is actually forward client data to server
+        // and send server's data back to client
+        else {
+            if (cf == null) {
+                // build a connection
+                Bootstrap bootstrap = new Bootstrap();
+                bootstrap.group(ctx.channel().eventLoop())
+                        .channel(ctx.channel().getClass())
+                        .handler(new ChannelInitializer<Channel>() {
+
+                            @Override
+                            protected void initChannel(Channel ch) {
+                                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx0, Object msg) {
+                                        System.out.println("HTTPS >>>> forwarding server's response back to client " +
+                                                ctx.channel().remoteAddress());
+                                        ctx.channel().writeAndFlush(msg);
+                                    }
+                                });
+                            }
+                        });
+                cf = bootstrap.connect(host, port);
+                cf.addListener((ChannelFutureListener) future -> {
+                    if (future.isSuccess()) {
+                        System.out.println("HTTPS created cf successfully >>>> writing to " + future.channel().remoteAddress());
+                        future.channel().writeAndFlush(msg);
+                    } else {
+                        ctx.channel().close();
+                    }
+                });
+            }
+            else {
+                System.out.println("HTTPS cf already exists >>>> writing to " + cf.channel().remoteAddress());
+                cf.channel().writeAndFlush(msg);
+            }
+        }
+    }
+}
